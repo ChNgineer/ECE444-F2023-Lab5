1052139d3cc4f7cda0619802fef7974056ddc726
Ryan Chen <ry4nch3n@gmail.com>
2023-10-27 04:15:00 -0400

    added flake8 and black, updated README.md

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
 # ECE444-F2023-Lab5
+
+# First Test
+
+![Alt text](images/test1.png)
+
+![Alt text](images/test1_pass.png)
+
+# Database Tests
+
+![Alt text](images/test2.png)
+
+![Alt text](images/test2_pass.png)
+
+# Templates and Views + Login Tests
+
+![Alt text](images/test3.png)
+
+![Alt text](images/test3-1.png)
+
+![Alt text](images/test3_pass.png)
+
+# JavaScript Implementation Tests
+
+![Alt text](images/test4_pass.png)
+
+# SQLAlchemy Tests
+
+![Alt text](images/test5_pass.png)
+
+# Login Required Final Test
+
+![Alt text](images/test6_pass.png)
+
+# Pros and Cons of TDD
+
+TODO
--- tests/app_test.py
+++ tests/app_test.py
@@ -6,6 +6,7 @@ import json
 
 TEST_DB = "test.db"
 
+
 @pytest.fixture
 def client():
     BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,23 +19,31 @@ def client():
         yield app.test_client()
         db.drop_all()
 
+
 def login(client, username, password):
-    return client.post("/login", data=dict(username=username, password=password), follow_redirects=True)
+    return client.post(
+        "/login", data=dict(username=username, password=password), follow_redirects=True
+    )
+
 
 def logout(client):
     return client.get("/logout", follow_redirects=True)
+
 
 def test_index(client):
     response = client.get("/", content_type="html/text")
     assert response.status_code == 200
 
+
 def test_database(client):
     tester = Path("test.db").is_file()
     assert tester
 
+
 def test_empty_db(client):
     rv = client.get("/")
     assert b"No entries yet. Add some!" in rv.data
+
 
 def test_login_logout(client):
     rv = login(client, app.config["USERNAME"], app.config["PASSWORD"])
@@ -46,21 +55,28 @@ def test_login_logout(client):
     rv = login(client, app.config["USERNAME"], app.config["PASSWORD"] + "x")
     assert b"Invalid password" in rv.data
 
+
 def test_messages(client):
     login(client, app.config["USERNAME"], app.config["PASSWORD"])
-    rv = client.post("/add", data=dict(title="<Hello>", text="<strong>HTML</strong> allowed here"), follow_redirects=True)
+    rv = client.post(
+        "/add",
+        data=dict(title="<Hello>", text="<strong>HTML</strong> allowed here"),
+        follow_redirects=True,
+    )
     assert b"No entries here so far" not in rv.data
     assert b"&lt;Hello&gt" in rv.data
     assert b"<strong>HTML</strong> allowed here" in rv.data
 
+
 def test_delete_message(client):
-    rv = client.get('/delete/1')
+    rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 0
     login(client, app.config["USERNAME"], app.config["PASSWORD"])
     rv = client.get("/delete/1")
     data = json.loads(rv.data)
     assert data["status"] == 1
+
 
 def test_search(client):
     pass
