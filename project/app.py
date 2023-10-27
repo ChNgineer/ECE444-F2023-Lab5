e8ed1b3b934bc273f3846a3f55fc113fe377105d
Ryan Chen <ry4nch3n@gmail.com>
2023-10-27 03:02:02 -0400

    completed user login

--- project/app.py
+++ project/app.py
@@ -1,8 +1,11 @@
 import sqlite3
-from flask import Flask, g
+from flask import Flask, g, render_template, request, session, flash, redirect, url_for, abort
 
 
 DATABASE = "flaskr.db"
+USERNAME = "admin"
+PASSWORD = "password"
+SECRET_KEY = "key"
 
 app = Flask(__name__)
 
@@ -35,9 +38,45 @@ def cloes_db(error):
         g.sqlite_db.close()
 
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('index'))
+
+
+@app.route('/add', methods=['POST'])
+def add_entry():
+    if not session.get('logged_in'):
+        abort(401)
+    db = get_db()
+    db.execute('insert into entries (title, text) values (?, ?)', [request.form['title'], request.form['text']])
+    db.commit()
+    flash('New entry was successfully posted')
+    return redirect(url_for('index'))
+
+
 @app.route("/")
-def hello():
-    return "Hello, World!"
+def index():
+    db = get_db()
+    cur = db.execute('select * from entries order by id desc')
+    entries = cur.fetchall()
+    return render_template('index.html', entries=entries)
 
 
 if __name__ == "__main__":
